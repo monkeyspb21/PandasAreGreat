a6c72985cdb7cd863c1c5d27dc178385aa3cbe15
Jordan Hudgens <jordan@devcamp.com>
2017-07-09 19:45:43 +0000

    Refactored before action for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
   
   def index
@@ -27,12 +28,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
-
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
@@ -44,21 +42,17 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
-  def destroy
-    # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
-
-    # Destroy/delete the record
-    @portfolio_item.destroy
-
-    # Redirect
-    respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    def destroy
+      # Perform the lookup
+      # Destroy/delete the record
+      @portfolio_item.destroy
+      # Redirect
+      respond_to do |format|
+        format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+      end
     end
-  end
 
   private
 
@@ -70,7 +64,8 @@ class PortfoliosController < ApplicationController
                                      )
   end
 
-
-
-
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 end
+
