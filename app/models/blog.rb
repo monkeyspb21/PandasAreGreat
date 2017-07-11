172d0f6d211e0da6881e54eee5cbb868a5fe66c2
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 14:41:16 +0000

    Updated styles for master layout file

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,13 +2,18 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
-  
-  validates_presence_of :title, :body
-  
+
+  validates_presence_of :title, :body, :topic_id
+
   belongs_to :topic
-  
+
+  has_many :comments, dependent: :destroy
+
   def self.special_blogs
     all
   end
-
+  
+  def self.recent
+    order("created_at DESC")
+  end
 end
