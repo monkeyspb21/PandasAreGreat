73b4670b4a8fc3aebaa4033f87cf3253c0098b31
Jordan Hudgens <jordan@devcamp.com>
2017-07-20 16:17:28 +0000

    Implemented persistent position updates for drag and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,22 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
   def index
     @portfolio_items = Portfolio.by_position
-    @page_title = "S. Baker Portfolio"
+  end
+
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+
+    render nothing: true
+  end
+
+  def angular
+    @angular_portfolio_items = Portfolio.angular
   end
 
   def new
@@ -29,7 +40,6 @@ class PortfoliosController < ApplicationController
   end
 
   def update
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -40,19 +50,17 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @page_title = @portfolio_item.title
-    @seo_keywords = @portfolio_item.body
   end
 
-    def destroy
-      # Perform the lookup
-      # Destroy/delete the record
-      @portfolio_item.destroy
-      # Redirect
-      respond_to do |format|
-        format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
-      end
+  def destroy
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
+  end
 
   private
 
