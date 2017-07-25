37e77d55e38713072fe6ff45c55b687d58f72607
Jordan Hudgens <jordan@devcamp.com>
2017-07-25 15:54:06 +0000

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,12 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
   accepts_nested_attributes_for :technologies, 
                                 reject_if: lambda {|attrs| attrs['name'].blank?}
+  include Placeholder
+  validates_presence_of :title, :body, :main_image, :thumb_image
+  
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
   
   def self.angular
     where(subtitle: 'Angular')
