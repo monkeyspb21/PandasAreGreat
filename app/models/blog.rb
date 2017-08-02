2e42e82971b37dd80ef6efee65329ebb0a3a6e74
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 15:40:13 +0000

    Refactored change to show more recent blogs, implemented function for only showing published blogs to users

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -8,7 +8,9 @@ class Blog < ApplicationRecord
   belongs_to :topic
   
   has_many :comments, dependent: :destroy
-
-  default_scope { order(created_at: :desc) }
+  
+  def self.recent
+    order("created_at DESC")
+  end
   
 end
