ace1abdfe7c9708a5e829d72adfecf53c23b95c7
Jordan Hudgens <jordan@devcamp.com>
2017-08-07 02:59:30 +0000

    Added users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,28 @@
-# 3.times do |topic|
-#  Topic.create!(
-#    title: "Topic #{topic}"
-#  )
-# end
+User.create!(
+  email: "test@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Regular User",
+)
+
+puts "1 regular user created"
+
+
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
 
 puts "3 Topics created"
 
