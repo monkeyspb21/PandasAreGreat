4715342ec093ceeedd843a6d15e0f9e7e1cc0ef9
Jordan Hudgens <jordan@devcamp.com>
2017-08-09 14:47:10 +0000

    updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 module SofiaAppProject
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
 
